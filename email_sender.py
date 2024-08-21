d5d705c73791144546a59b7317de05350200364f
Jason Tam <jasonyst93@gmail.com>
2024-08-21 19:03:55 +0800

    email sender

--- email_sender.py
+++ email_sender.py
@@ -1,23 +1,28 @@
-# Import smtplib for the actual sending function
 import smtplib
-
-# Import the email modules we'll need
 from email.message import EmailMessage
 
- # Create a text/plain message
-email = EmailMessage()
-email.set_content('Testing on emails sending')
+#Two more built-in libraries!!
+#Subsitle variable into text
+from string import Template
 
-# me == the sender's email address
-# you == the recipient's email address
+#Read/Access the HTML file
+from pathlib import Path #smiliar to os.path
+
+#Access(Path) and read the text(.read_text()) from index.html and transfer to a Template object(Template) 
+html = Template(Path('index.html').read_text())
+
+ # set content to a Templated object (html) with substitue
+email = EmailMessage()
+email.set_content(html.substitute({'name': 'Hanni'}),'html') # substitute name = Hanni and tell it's html
+
 email['Subject'] = 'Testing on emails sending via Python'
 email['From'] = "Jason Tam"
 email['To'] = 'jasonyst93@gmail.com'
 
 
 with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
-    smtp.ehlo() #sending hello
-    smtp.starttls() #encripy 
-    smtp.login('jasonyst93test@gmail.com','oxah apbd cjlm exph') #login with app password (youtube)
-    smtp.send_message(email) #sending an email
+    smtp.ehlo() 
+    smtp.starttls() 
+    smtp.login('jasonyst93test@gmail.com','oxah apbd cjlm exph') 
+    smtp.send_message(email)
 
